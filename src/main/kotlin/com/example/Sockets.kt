6f793a7e63ccce2ceb99970b03cdfad27363e595
Kondzitsu <Konkord720@gmail.com>
2024-12-19 01:09:38 +0100

    Example application using webSockets with Ktor

---
+++ src/main/kotlin/com/example/Sockets.kt
@@ -0,0 +1,54 @@
+package com.example
+
+import io.ktor.serialization.kotlinx.*
+import io.ktor.server.application.*
+import io.ktor.server.routing.*
+import io.ktor.server.websocket.*
+import io.ktor.websocket.*
+import kotlinx.coroutines.delay
+import kotlinx.serialization.json.Json
+import kotlin.time.Duration.Companion.seconds
+import model.*
+import java.util.*
+import kotlin.collections.ArrayList
+
+fun Application.configureSockets() {
+    install(WebSockets) {
+        contentConverter = KotlinxWebsocketSerializationConverter(Json)
+        pingPeriod = 15.seconds
+        timeout = 60.seconds
+        maxFrameSize = Long.MAX_VALUE
+        masking = false
+    }
+    routing {
+        val sessions =
+            Collections.synchronizedList<WebSocketServerSession>(ArrayList())
+
+        webSocket("/tasks") {
+            sendAllTasks()
+            close(CloseReason(CloseReason.Codes.NORMAL, "All done"))
+        }
+
+        webSocket("/tasks2") {
+            sessions.add(this)
+            sendAllTasks()
+
+            while(true) {
+                val newTask = receiveDeserialized<Task>()
+                TaskRepository.addTask(newTask)
+                println(this)
+                for(session in sessions) {
+                    println(session)
+                    session.sendSerialized(newTask)
+                }
+            }
+        }
+    }
+}
+
+private suspend fun DefaultWebSocketServerSession.sendAllTasks() {
+    for (task in TaskRepository.allTasks()) {
+        sendSerialized(task)
+        delay(1000)
+    }
+}
